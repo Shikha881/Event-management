05a9caba9200ed960a53ea87dc043a52644d267a
Shikha881 <shikhananda730@gmail.com>
2025-04-21 13:42:58 +0530

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Event Management System</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <h2>Welcome to the Event Management System</h2>
+    <a href="register.php">Register</a> | <a href="login.php">Login</a>
